d91839a21d1c1aeec85c43c6386db3948deef0f3
h0r5t <h0r5t@h0r5t.com>
2015-09-13 01:10:11 +0200

    changed AttrapCrawler

--- src/crawler/AtrapCrawler.py
+++ src/crawler/AtrapCrawler.py
@@ -1,6 +1,10 @@
+from crawler.Dota2ApiWrapper import Dota2ApiWrapper
+
+
 class AtrapCrawler:
     def __init__(self):
         api_key = self.loadApiKey()
+        api_wrapper = Dota2ApiWrapper(api_key)
 
     def loadApiKey(self):
-        pass
+        print("hi")
