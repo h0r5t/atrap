02795bb1d43cae805ef7132e585b336681e0bf0c
h0r5t <h0r5t@h0r5t.com>
2015-09-20 15:11:42 +0200

    improved overview page

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -7,6 +7,12 @@ atrapControllers.controller('LiveOverviewCtrl', ['$scope', '$location', 'AllPlay
     $scope.getPlayerUrl = function(playerID) {
         return $location.absUrl() + '/players/' + String(playerID);
     };
+    $scope.getPosStyle = function(playerPos) {
+        if (playerPos == "carry") return "#92B5F2";
+        else if (playerPos == "mid") return "#FF8080";
+        else if (playerPos == "offlane") return "#FFFF66";
+        else if (playerPos == "support") return "#855C33";
+    };
     $scope.playerIDs = AllPlayers.get(function (player_list) {
       for (var i = 0; i < player_list.all_players.length; i++) {
         var id_a = parseInt(player_list.all_players[i]);
